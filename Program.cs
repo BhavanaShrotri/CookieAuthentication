76704327dbf71d09018395543c9edd64d964b579
Bhavana Shrotri <Bhavana.Shrotri@Klingelnberg.com>
2023-01-25 18:06:08 +0530

    using javascript page

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication()
-    .AddCookie("default");
+    .AddCookie("default",
+    o =>
+    {
+        o.Cookie.Name = "mycookie";
+    });
 
 builder.Services.AddControllers();
 
@@ -11,6 +18,22 @@ app.UseStaticFiles();
 app.UseAuthentication();
 
 app.MapGet("/", () => "Hello World");
+
+app.MapPost("/login", async (HttpContext ctx) =>
+{
+    await ctx.SignInAsync("default", new ClaimsPrincipal(
+               new ClaimsIdentity(
+                   new Claim[]
+                   {
+                        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                   },
+                   "default"
+                   )
+               ));
+
+    return "Ok";
+});
+
 app.MapDefaultControllerRoute();
 
 app.Run();
